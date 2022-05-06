894694a1866f083c5997ce12c264e5887ed0c8fd
emansami23 <100478288+emansami23@users.noreply.github.com>
2022-05-06 20:17:07 +0200

    Add files via upload

---
+++ project without Ai/main.py
@@ -0,0 +1,44 @@
+from ast import Pass
+from lib2to3.pygram import python_grammar_no_print_statement
+import pygame
+from checkers.constants import RED, WHITE, WIDTH, HEIGHT, SQUARE_SIZE
+from checkers.piece import Piece
+from checkers.game import Game
+
+FPS = 60
+
+WIN =pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption('Checkers')
+
+def get_row_col_from_mouse(pos):
+    x, y = pos
+    row = y // SQUARE_SIZE
+    col = x // SQUARE_SIZE
+    return row, col
+
+def main():
+    run = True
+    clock = pygame.time.Clock()
+    game = Game(WIN)
+    
+
+    while run:
+       
+        if game.winner() != None:
+            print(game.winner())
+            run = False
+
+        for event in pygame.event.get():
+           if event.type == pygame.QUIT:
+               run = False
+
+           if event.type == pygame.MOUSEBUTTONDOWN:
+                Pos = pygame.mouse.get_pos()
+                row, col = get_row_col_from_mouse(Pos)
+                game.select(row, col)
+
+        game.update()   
+    
+    pygame.quit()
+
+main()
